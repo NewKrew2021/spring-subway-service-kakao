a5c72635391976f764f300f91f62e699433e7559
boorownie <probitanima11@gmail.com>
2021-01-06 20:32:43 +0900

    feat(service): sample code

--- src/main/java/subway/PageController.java
+++ src/main/java/subway/PageController.java
@@ -4,9 +4,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * 이 클래스는 프론트엔드 코드에서 활용하는 코드 입니다. 수정하지 마세요.
- */
 @Controller
 public class PageController {
     @GetMapping(value = {
