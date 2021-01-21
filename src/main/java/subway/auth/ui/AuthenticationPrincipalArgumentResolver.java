b105ca291d24b9891d510230e0e00273e004d191
waylon <waylon.n@kakaocorp.com>
2021-01-21 18:56:23 +0900

    feat: AuthenticationPrincipalArgumentResolver Configuration

--- src/main/java/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -1,6 +1,7 @@
 package subway.auth.ui;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -11,6 +12,7 @@ import subway.auth.infrastructure.AuthorizationExtractor;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Component
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
     private AuthService authService;
 
