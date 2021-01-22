7e922e77bf4d1426e7a0224995f4fd0643c64f51
hendo.park <hendo.park@kakaocorp.com>
2021-01-22 17:25:50 +0900

    Changes by kuri

--- src/main/java/subway/path/dto/PathResponse.java
+++ src/main/java/subway/path/dto/PathResponse.java
@@ -4,6 +4,7 @@ import subway.station.domain.Station;
 import subway.station.dto.StationResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathResponse {
     private List<StationResponse> stations;
@@ -18,6 +19,16 @@ public class PathResponse {
         this.distance = distance;
         this.fare = fare;
     }
+
+    public static PathResponse make(List<Station> stations, int distance, int fare){
+        return new PathResponse(stations.stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList()),
+                distance,
+                fare);
+    }
+
+
 
     public List<StationResponse> getStations() {
         return stations;
