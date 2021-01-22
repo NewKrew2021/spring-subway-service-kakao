b485f675698aae19e81e3f45103014c221745fbe
joy.smile <joy@joysmileui-MacBookPro.local>
2021-01-22 21:27:31 +0900

    feat(favorite): 즐겨찾기 CRUD 구현

---
+++ src/main/java/subway/favorite/domain/Favorite.java
@@ -0,0 +1,38 @@
+package subway.favorite.domain;
+
+import subway.station.domain.Station;
+
+public class Favorite {
+
+    private Long id;
+    private Long memberId;
+    private Station sourceStation;
+    private Station targetStation;
+
+    public Favorite(Long id, Long memberId, Station sourceStation, Station targetStation) {
+        this.id = id;
+        this.memberId = memberId;
+        this.sourceStation = sourceStation;
+        this.targetStation = targetStation;
+    }
+
+    public Favorite(Long memberId, Station sourceStation, Station targetStation) {
+        this(null, memberId, sourceStation, targetStation);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Station getSourceStation() {
+        return sourceStation;
+    }
+
+    public Station getTargetStation() {
+        return targetStation;
+    }
+}
