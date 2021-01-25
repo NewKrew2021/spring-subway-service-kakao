e9c8827fb1171454acd1742c0e5d267a4e30352b
jack.won <jack.won@kakaocorp.com>
2021-01-25 19:16:21 +0900

    refactor(Exception): 예외 처리 추가

--- src/main/java/subway/exception/ExceptionAdvice.java
+++ src/main/java/subway/exception/ExceptionAdvice.java
@@ -9,6 +9,16 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(LoginFailException.class)
     public ResponseEntity<String> handleDataAccessException(Exception e) {
-        return ResponseEntity.status(401).body("잘못된 입력입니다.");
+        return ResponseEntity.status(401).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(Exception e){
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e){
+        return ResponseEntity.status(400).body("잘못된 요청입니다.");
     }
 }
