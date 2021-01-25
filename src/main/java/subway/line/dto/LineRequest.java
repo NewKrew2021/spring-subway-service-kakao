54dcb4a7a9de8e878c7bdd5754bfb71c145172ba
brody <youoper@gmail.com>
2021-01-25 16:30:30 +0900

    refactor(Line): extraFare 필드 추가

--- src/main/java/subway/line/dto/LineRequest.java
+++ src/main/java/subway/line/dto/LineRequest.java
@@ -12,11 +12,7 @@ public class LineRequest {
     }
 
     public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
-        this.name = name;
-        this.color = color;
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
-        this.distance = distance;
+        this(name, color, upStationId, downStationId, distance, 0);
     }
 
     public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance, int extraFare) {
@@ -46,5 +42,9 @@ public class LineRequest {
 
     public int getDistance() {
         return distance;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
     }
 }
--- src/main/java/subway/line/dto/LineResponse.java
+++ src/main/java/subway/line/dto/LineResponse.java
@@ -18,9 +18,14 @@ public class LineResponse {
     }
 
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+        this(id, name, color, 0, stations);
+    }
+
+    public LineResponse(Long id, String name, String color, int extraFare, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.extraFare = extraFare;
         this.stations = stations;
     }
 
@@ -28,7 +33,7 @@ public class LineResponse {
         List<StationResponse> stations = line.getStations().stream()
                 .map(it -> StationResponse.of(it))
                 .collect(Collectors.toList());
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getExtraFare(), stations);
     }
 
     public static List<LineResponse> listOf(List<Line> lines) {
@@ -47,6 +52,10 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
     }
 
     public List<StationResponse> getStations() {
