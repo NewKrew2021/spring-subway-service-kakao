6df72c406c70177850551d227697121689d126db
brody <youoper@gmail.com>
2021-01-25 18:00:04 +0900

    feat(Path): extra Line fare 구현

--- src/main/java/subway/path/application/PathService.java
+++ src/main/java/subway/path/application/PathService.java
@@ -15,6 +15,7 @@ import subway.path.dto.PathResponse;
 import subway.station.application.StationService;
 import subway.station.domain.Station;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,23 +32,7 @@ public class PathService {
 
     public PathResponse getPath(Long sourceStationId, Long targetStationId) {
 
-        List<Line> lines = lineService.findLines();
-        Sections sections = new Sections();
-
-        for (Line line : lines) {
-            sections.addSections(line.getSections());
-        }
-
-        List<Station> stations = stationService.findAll();
-
-
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
-
-        stations.forEach(graph::addVertex);
-
-        for (Section section : sections.getSections()) {
-            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
-        }
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = makeGraph();
 
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
 
@@ -57,8 +42,71 @@ public class PathService {
                 stationService.findStationById(targetStationId)
         );
 
-        Path path = new Path(graphPath.getVertexList(), (int) graphPath.getWeight());
+        List<Station> stations = graphPath.getVertexList();
+
+        int fare = 1250;
+
+        fare += findMaxLineFare(stations);
+
+        Path path = new Path(graphPath.getVertexList(), (int) graphPath.getWeight(), fare);
 
         return PathResponse.of(path);
+    }
+
+    private int findMaxLineFare(List<Station> stations) {
+
+        int maxLineFare = 0;
+
+        for (int i = 0; i < stations.size() - 1; i++) {
+            int lineFare = findLineFare(stations.get(i), stations.get(i + 1));
+
+            if (maxLineFare < lineFare) {
+                maxLineFare = lineFare;
+            }
+        }
+
+        return maxLineFare;
+
+    }
+
+    private int findLineFare(Station source, Station target) {
+        List<Line> lines = lineService.findLines();
+
+        int minimumDistance = Integer.MAX_VALUE;
+        int lineFare = Integer.MAX_VALUE;
+
+        for (Line line : lines) {
+            Sections sections = line.getSections();
+            for (Section section : sections.getSections()) {
+                if (
+                        ( target.equals(section.getDownStation())
+                                && source.equals(section.getUpStation()) )
+                        || ( source.equals(section.getDownStation())
+                                && target.equals(section.getUpStation()) ) )  {
+                    if (section.getDistance() <= minimumDistance) {
+
+                        lineFare = Integer.min(lineFare, line.getExtraFare());
+                    }
+                }
+            }
+        }
+        return lineFare;
+    }
+
+    private WeightedMultigraph<Station, DefaultWeightedEdge> makeGraph() {
+
+        Sections sections = lineService.findAllSections();
+
+        List<Station> stations = stationService.findAll();
+
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+
+        stations.forEach(graph::addVertex);
+
+        for (Section section : sections.getSections()) {
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
+
+        return graph;
     }
 }
