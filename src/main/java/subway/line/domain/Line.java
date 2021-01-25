daec18d7812a5898aa2fd39a27ba66bfb706a755
wonsik <lgock2023@gmail.com>
2021-01-25 16:00:16 +0900

    feat(Line): extraFare를 멤버 변수로 추가

--- src/main/java/subway/line/domain/Line.java
+++ src/main/java/subway/line/domain/Line.java
@@ -8,6 +8,7 @@ public class Line {
     private Long id;
     private String name;
     private String color;
+    private int extraFare;
     private Sections sections = new Sections();
 
     public Line() {
@@ -19,15 +20,27 @@ public class Line {
     }
 
     public Line(Long id, String name, String color) {
+        this(name, color);
         this.id = id;
-        this.name = name;
-        this.color = color;
+    }
+
+    public Line(String name, String color, int extraFare) {
+        this(name, color);
+        this.extraFare = extraFare;
+    }
+
+    public Line(Long id, String name, String color, int extraFare) {
+        this(name, color, extraFare);
+        this.id = id;
     }
 
     public Line(Long id, String name, String color, Sections sections) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
+        this(id, name, color);
+        this.sections = sections;
+    }
+
+    public Line(Long id, String name, String color, int extraFare, Sections sections) {
+        this(id, name, color, extraFare);
         this.sections = sections;
     }
 
@@ -41,6 +54,10 @@ public class Line {
 
     public String getColor() {
         return color;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
     }
 
     public Sections getSections() {
