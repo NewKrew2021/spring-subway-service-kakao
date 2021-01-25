dbf72eb704558e7d1b59bd684432f659bdbad9c3
wonsik <lgock2023@gmail.com>
2021-01-25 16:49:53 +0900

    feat(favorite): 2단계 즐겨찾기 추가

--- src/main/java/subway/favorite/dto/FavoriteResponse.java
+++ src/main/java/subway/favorite/dto/FavoriteResponse.java
@@ -1,11 +1,35 @@
 package subway.favorite.dto;
 
+import subway.favorite.domain.Favorite;
 import subway.station.dto.StationResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FavoriteResponse {
     private Long id;
     private StationResponse source;
     private StationResponse target;
+
+    public FavoriteResponse(Long id, StationResponse source, StationResponse target) {
+        this.id = id;
+        this.source = source;
+        this.target = target;
+    }
+
+    public static FavoriteResponse of(Favorite favorite) {
+        return new FavoriteResponse(
+                favorite.getId(),
+                StationResponse.of(favorite.getSourceStation()),
+                StationResponse.of(favorite.getTargetStation())
+        );
+    }
+
+    public static List<FavoriteResponse> listOf(List<Favorite> favorites) {
+        return favorites.stream()
+                .map(FavoriteResponse::of)
+                .collect(Collectors.toList());
+    }
 
     public Long getId() {
         return id;
@@ -18,4 +42,5 @@ public class FavoriteResponse {
     public StationResponse getTarget() {
         return target;
     }
+
 }
