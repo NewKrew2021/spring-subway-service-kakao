609efd2c4baf3e712ac982b88c94f0cbc126f052
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-26 23:17:36 +0900

    refactor(Station) : StationQuery Enum 클래스 정의

---
+++ src/main/java/subway/station/dao/StationQuery.java
@@ -0,0 +1,18 @@
+package subway.station.dao;
+
+public enum StationQuery {
+    FIND_ALL("select * from STATION"),
+    FIND_BY_ID("select * from STATION where id = ?"),
+    DELETE("delete from STATION where id = ?")
+    ;
+
+    private String query;
+
+    StationQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
