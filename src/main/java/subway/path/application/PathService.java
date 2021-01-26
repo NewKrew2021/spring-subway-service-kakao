c340e066c092d5999d5a01aed243216140b1db55
= <huey.j@kakaocorp.com>
2021-01-26 16:08:11 +0900

    refactor : Lines 구현 및 다익스트라 관련 역할 이전

--- src/main/java/subway/path/application/PathService.java
+++ src/main/java/subway/path/application/PathService.java
@@ -1,16 +1,13 @@
 package subway.path.application;
 
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import org.springframework.stereotype.Service;
 import subway.line.application.LineService;
-import subway.line.domain.Line;
+import subway.line.domain.Lines;
 import subway.member.domain.AGE;
 import subway.path.domain.Vertex;
-import subway.path.domain.strategy.FareStrategy;
 import subway.path.domain.strategy.AgeFare;
 import subway.path.domain.strategy.DistanceFare;
+import subway.path.domain.strategy.FareStrategy;
 import subway.path.domain.strategy.LineFare;
 import subway.path.dto.PathResponse;
 import subway.station.application.StationService;
@@ -21,6 +18,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class PathService {
+    public static final int DEFAULT_FARE = 0;
     private final StationService stationService;
     private final LineService lineService;
 
@@ -30,14 +28,13 @@ public class PathService {
     }
 
     public PathResponse getShortPath(Long sourceId, Long targetId, AGE age) {
-        List<Line> lines = lineService.findLines();
-        DijkstraShortestPath dijkstraShortestPath = getDijkstraShortestPath(lines);
-        List<Vertex> vertexs = dijkstraShortestPath.getPath(Vertex.of(sourceId), Vertex.of(targetId)).getVertexList();
+        Lines lines = Lines.of(lineService.findLines());
+        List<Vertex> vertexs = lines.getVertexs(sourceId, targetId);
 
-        int distance = (int) dijkstraShortestPath.getPathWeight(Vertex.of(sourceId), Vertex.of(targetId));
-        int fare = 0;
+        int distance = lines.getDistance(sourceId, targetId);
+
         FareStrategy fareStrategy = new DistanceFare(distance);
-        fare = fareStrategy.apply(fare);
+        int fare = fareStrategy.apply(DEFAULT_FARE);
 
         fareStrategy = new LineFare(vertexs);
         fare = fareStrategy.apply(fare);
@@ -52,14 +49,5 @@ public class PathService {
         return vertexs.stream()
                 .map(vertex -> StationResponse.of(stationService.findStationById(vertex.getStationId())))
                 .collect(Collectors.toList());
-    }
-
-    private DijkstraShortestPath getDijkstraShortestPath(List<Line> lines) {
-        WeightedMultigraph<Vertex, DefaultWeightedEdge> graph
-                = new WeightedMultigraph(DefaultWeightedEdge.class);
-        for (Line line : lines) {
-            line.fillPath(graph);
-        }
-        return new DijkstraShortestPath(graph);
     }
 }
