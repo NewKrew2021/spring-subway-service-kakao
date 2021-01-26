41a53b1c352e115fc67384e68ff9435fe505b249
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-26 23:09:34 +0900

    refactor(path) : DiscountAge 정의 및 리팩토링
    - 할인할 수 있는 나이를 enum 클래스로 정의

--- src/main/java/subway/path/application/PathService.java
+++ src/main/java/subway/path/application/PathService.java
@@ -2,11 +2,8 @@ package subway.path.application;
 
 import org.springframework.stereotype.Service;
 import subway.line.dao.LineDao;
-import subway.line.dao.SectionDao;
-import subway.line.domain.Line;
 import subway.line.domain.Lines;
-import subway.line.domain.Section;
-import subway.line.domain.Sections;
+import subway.path.domain.DiscountAge;
 import subway.path.domain.DistanceExtraFare;
 import subway.path.domain.SubwayPathGraph;
 import subway.path.dto.PathResponse;
@@ -15,60 +12,46 @@ import subway.station.domain.Station;
 import subway.station.dto.StationResponse;
 
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Service
 public class PathService {
-    private final SectionDao sectionDao;
+    private final int EXTRA_FARE_DEFAULT = 0;
     private final StationDao stationDao;
     private final LineDao lineDao;
 
-    public PathService(SectionDao sectionDao, StationDao stationDao, LineDao lineDao) {
-        this.sectionDao = sectionDao;
+    public PathService(StationDao stationDao, LineDao lineDao) {
         this.stationDao = stationDao;
         this.lineDao = lineDao;
     }
 
-    public PathResponse getShortestpathResponse(Long source, Long target, int age) {
+    public PathResponse getShortestPathResponse(Long source, Long target, int age) {
         SubwayPathGraph subwayPathGraph = new SubwayPathGraph(
                 new Lines(lineDao.findAll()), source, target);
 
         List<StationResponse> shortestStations = subwayPathGraph.getVertexList()
                 .stream()
                 .map((String stationId) -> stationDao.findById(Long.parseLong(stationId)))
-                .map((Station station) -> new StationResponse(station.getId(), station.getName()))
+                .map((Station station) -> StationResponse.of(station))
                 .collect(Collectors.toList());
 
         int totalFare = DistanceExtraFare.getTotalFare(subwayPathGraph.getTotalDistance())
-                + getExtraFareByLines(subwayPathGraph.getLineIdsInShortestPath());
-        totalFare = getAgeDiscountTotalFare(totalFare, age);
+                + addExtraFareByLines(subwayPathGraph.getLineIdsInShortestPath());
+
+        totalFare = DiscountAge.getTotalFareToDiscountAge(totalFare,age);
 
         return new PathResponse(shortestStations,
                 subwayPathGraph.getTotalDistance(), totalFare);
     }
 
-
-    private int getExtraFareByLines(List<Long> lineIds){
-        int maxExtratotalExtra = 0;
+    private int addExtraFareByLines(List<Long> lineIds){
+        int maxLineExtraFare = EXTRA_FARE_DEFAULT;
 
         for(Long lineId : lineIds){
-            maxExtratotalExtra = Math.max(lineDao.findById(lineId).getExtraFare(), maxExtratotalExtra);
+            maxLineExtraFare = Math.max(lineDao.findById(lineId).getExtraFare(), maxLineExtraFare);
         }
 
-        return maxExtratotalExtra;
+        return maxLineExtraFare;
     }
 
-    private int getAgeDiscountTotalFare(int totalFare, int age){
-        if(13 <= age && age <= 18){
-            int discountAmount = (totalFare - 350) / 100 * 20;
-            totalFare -= discountAmount;
-        }
-        if(6 <= age && age <= 12){
-            int discountAmount = (totalFare - 350) / 100 * 50;
-            totalFare -= discountAmount;
-        }
-        return totalFare;
-    }
 }
---
+++ src/main/java/subway/path/domain/DiscountAge.java
@@ -0,0 +1,45 @@
+package subway.path.domain;
+
+public enum DiscountAge {
+    BABY(1, 5, 1),
+    CHILD(6, 12, 0.5),
+    YOUTH(13, 19, 0.2);
+
+    private static final int BABY_FARE = 0;
+    private static final int DEDUCTION_FARE = 350;
+
+    private int minAge;
+    private int maxAge;
+    private double discountRate;
+
+    DiscountAge(int minAge, int maxAge, double discountRate) {
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+        this.discountRate = discountRate;
+    }
+
+    public static int getTotalFareToDiscountAge(int totalFare, int age) {
+        if (BABY.isAge(age)) {
+            return BABY_FARE;
+        }
+
+        if (CHILD.isAge(age)) {
+            return totalFare - CHILD.getDiscountAmount(totalFare);
+        }
+
+        if (YOUTH.isAge(age)) {
+            return totalFare - YOUTH.getDiscountAmount(totalFare);
+        }
+
+        return totalFare;
+    }
+
+    private int getDiscountAmount(int totalFare){
+        return (int) ((totalFare - DEDUCTION_FARE) * this.discountRate);
+    }
+
+    private boolean isAge(int age) {
+        return this.minAge <= age && age <= this.maxAge;
+    }
+
+}
