aae331fef37a93cc4b3ebf62bd8be4f70181ccf3
tars.case <tars.case@kakaocorp.com>
2021-01-26 16:27:00 +0900

    refactor : pathTest 수정

--- src/test/java/subway/path/domain/PathTest.java
+++ src/test/java/subway/path/domain/PathTest.java
@@ -3,6 +3,9 @@ package subway.path.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import subway.line.domain.Section;
 import subway.line.domain.SectionWithFare;
 import subway.line.domain.SectionsInAllLine;
@@ -15,13 +18,13 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.*;
 
 public class PathTest {
-    Station 강남역 = new Station(1L, "강남역");
-    Station 망포역 = new Station(2L, "망포역");
-    Station 역삼역 = new Station(3L, "역삼역");
-    Station 수원역 = new Station(4L, "수원역");
-    Station 판교역 = new Station(5L, "판교역");
-    Station 제주역 = new Station(6L, "제주역");
-    Station 서귀포역 = new Station(7L, "서귀포역");
+    static Station 강남역 = new Station(1L, "강남역");
+    static Station 망포역 = new Station(2L, "망포역");
+    static Station 역삼역 = new Station(3L, "역삼역");
+    static Station 수원역 = new Station(4L, "수원역");
+    static Station 판교역 = new Station(5L, "판교역");
+    static Station 제주역 = new Station(6L, "제주역");
+    static Station 서귀포역 = new Station(7L, "서귀포역");
 
     Section 강남_망포 = new Section(1L, 강남역, 망포역, 5);
     Section 망포_역삼 = new Section(2L, 망포역, 역삼역, 5);
@@ -49,9 +52,17 @@ public class PathTest {
         assertThat(path.getStations()).containsExactly(강남역, 망포역, 수원역);
     }
 
-    @Test
-    void failToFindShortestPathTest() {
-        assertThatThrownBy(() -> pathGraph.getPath(강남역, 서귀포역)).isInstanceOf(InvalidPathException.class);
-        assertThatThrownBy(() -> pathGraph.getPath(수원역, 제주역)).isInstanceOf(InvalidPathException.class);
+    @DisplayName("연결되지 않은 역들의 경로를 조회한다.")
+    @ParameterizedTest
+    @MethodSource("provideStation")
+    void failToFindShortestPathTest(Station source, Station destination) {
+        assertThatThrownBy(() -> pathGraph.getPath(source, destination)).isInstanceOf(InvalidPathException.class);
+    }
+
+    private static Stream<Arguments> provideStation() {
+        return Stream.of(
+                Arguments.of(강남역, 서귀포역),
+                Arguments.of(수원역, 제주역)
+        );
     }
 }
