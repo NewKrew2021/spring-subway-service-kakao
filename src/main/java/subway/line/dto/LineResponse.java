fba05f39a420a26c874a9eb8541990a93afda8d9
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-26 23:48:00 +0900

    refactor(line) : lineResponse 생성자에 extraFare 적용

--- src/main/java/subway/line/dto/LineResponse.java
+++ src/main/java/subway/line/dto/LineResponse.java
@@ -17,18 +17,19 @@ public class LineResponse {
 
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, int extraFare) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.stations = stations;
+        this.extraFare = extraFare;
     }
 
     public static LineResponse of(Line line) {
         List<StationResponse> stations = line.getStations().stream()
                 .map(it -> StationResponse.of(it))
                 .collect(Collectors.toList());
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getExtraFare());
     }
 
     public static List<LineResponse> listOf(List<Line> lines) {
