8657b1d8cb22f58b1e02cf07f4e1cf812046e22c
teo.kkim <teo.kkim@kakaocorp.com>
2021-01-28 01:25:22 +0900

    refactor: Mock을 사용하여 테스트 코드 정리

--- src/test/java/subway/path/domain/PathBuilderTest.java
+++ src/test/java/subway/path/domain/PathBuilderTest.java
@@ -3,26 +3,54 @@ package subway.path.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import subway.AcceptanceTest;
 import subway.line.dao.LineDao;
+import subway.line.domain.Line;
+import subway.line.domain.Section;
+import subway.line.domain.Sections;
 import subway.station.dao.StationDao;
+import subway.station.domain.Station;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class PathBuilderTest {
-
-    @Autowired
-    private StationDao stationDao;
-
-    @Autowired
-    private LineDao lineDao;
-
+class PathBuilderTest extends AcceptanceTest {
+    StationDao stationDao = mock(StationDao.class);
+    LineDao lineDao = mock(LineDao.class);
     private PathBuilder pathBuilder;
 
     @BeforeEach
-    public void setUp() {
+    public void before() {
+        Station 강남역 = new Station(1L, "강남역");
+        Station 판교역 = new Station(2L, "판교역");
+        Station 정자역 = new Station(3L, "정자역");
+        Station 역삼역 = new Station(4L, "역삼역");
+        Station 잠실역 = new Station(5L, "잠실역");
+
+        List<Station> stations = Arrays.asList(강남역, 판교역, 정자역, 역삼역, 잠실역);
+        when(stationDao.findAll()).thenReturn(stations);
+
+        when(lineDao.findAll()).thenReturn(
+                Arrays.asList(
+                        new Line(1L, "신분당선", "Red", 0, new Sections(
+                                Arrays.asList(
+                                        new Section(강남역, 판교역, 10),
+                                        new Section(판교역, 정자역, 10)
+                                )
+                        )),
+                        new Line(2L, "2호선", "GREEN", 0, new Sections(
+                                Arrays.asList(
+                                        new Section(강남역, 역삼역, 10),
+                                        new Section(역삼역, 잠실역, 10)
+                                )
+                        ))
+                )
+        );
+
         pathBuilder = new PathBuilder()
                 .initializePath(stationDao.findAll(), lineDao.findAll())
                 .makeGraphPath(3L, 5L);
