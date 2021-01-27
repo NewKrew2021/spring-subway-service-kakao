bc9684698bcfc387f7f454dc8f1888efc5436f43
dddfff22 <dddfff22@naver.com>
2021-01-27 21:47:47 +0900

    test: getAge 메서드 테스트 추가

--- src/test/java/subway/path/domain/DistanceFareTest.java
+++ src/test/java/subway/path/domain/DistanceFareTest.java
@@ -2,31 +2,31 @@ package subway.path.domain;
 
 import org.junit.jupiter.api.Test;
 
-import  static  org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DistanceFareTest {
 
 
     @Test
-    public void distanceFareTest(){
-        int distanceFare=DistanceFare.getDistanceFare(1);
+    public void distanceFareTest() {
+        int distanceFare = DistanceFare.getDistanceFare(1);
         assertThat(distanceFare).isEqualTo(1250);
-        distanceFare=DistanceFare.getDistanceFare(11);
+        distanceFare = DistanceFare.getDistanceFare(11);
         assertThat(distanceFare).isEqualTo(1350);
-        distanceFare=DistanceFare.getDistanceFare(15);
+        distanceFare = DistanceFare.getDistanceFare(15);
         assertThat(distanceFare).isEqualTo(1350);
-        distanceFare=DistanceFare.getDistanceFare(16);
+        distanceFare = DistanceFare.getDistanceFare(16);
         assertThat(distanceFare).isEqualTo(1450);
-        distanceFare=DistanceFare.getDistanceFare(49);
+        distanceFare = DistanceFare.getDistanceFare(49);
         assertThat(distanceFare).isEqualTo(2050);
-        distanceFare=DistanceFare.getDistanceFare(50);
+        distanceFare = DistanceFare.getDistanceFare(50);
         assertThat(distanceFare).isEqualTo(2050);
 
-        distanceFare=DistanceFare.getDistanceFare(51);
+        distanceFare = DistanceFare.getDistanceFare(51);
         assertThat(distanceFare).isEqualTo(2150);
-        distanceFare=DistanceFare.getDistanceFare(58);
+        distanceFare = DistanceFare.getDistanceFare(58);
         assertThat(distanceFare).isEqualTo(2150);
-        distanceFare=DistanceFare.getDistanceFare(59);
+        distanceFare = DistanceFare.getDistanceFare(59);
         assertThat(distanceFare).isEqualTo(2250);
     }
 
