89da45bbf5ff484d208ad258d788ffe57f726fed
teo.kkim <teo.kkim@kakaocorp.com>
2021-01-28 01:24:53 +0900

    refactor: SQL 중복 제거 및 상수화

--- src/main/java/subway/line/dao/LineDao.java
+++ src/main/java/subway/line/dao/LineDao.java
@@ -17,8 +17,19 @@ import java.util.stream.Collectors;
 
 @Repository
 public class LineDao {
-    private JdbcTemplate jdbcTemplate;
-    private SimpleJdbcInsert insertAction;
+    public static final String FIND_ALL_QUERY = "select L.id as line_id, L.name as line_name, L.color as line_color, L.extra_fare as extra_fare, " +
+            "S.id as section_id, S.distance as section_distance, " +
+            "UST.id as up_station_id, UST.name as up_station_name, " +
+            "DST.id as down_station_id, DST.name as down_station_name " +
+            "from LINE L \n" +
+            "left outer join SECTION S on L.id = S.line_id " +
+            "left outer join STATION UST on S.up_station_id = UST.id " +
+            "left outer join STATION DST on S.down_station_id = DST.id ";
+
+    public static final String FIND_BY_ID_QUERY = FIND_ALL_QUERY + " WHERE L.id = ?";
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertAction;
 
     public LineDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
@@ -39,36 +50,17 @@ public class LineDao {
     }
 
     public Line findById(Long id) {
-        String sql = "select L.id as line_id, L.name as line_name, L.color as line_color, L.extra_fare as extra_fare, " +
-                "S.id as section_id, S.distance as section_distance, " +
-                "UST.id as up_station_id, UST.name as up_station_name, " +
-                "DST.id as down_station_id, DST.name as down_station_name " +
-                "from LINE L \n" +
-                "left outer join SECTION S on L.id = S.line_id " +
-                "left outer join STATION UST on S.up_station_id = UST.id " +
-                "left outer join STATION DST on S.down_station_id = DST.id " +
-                "WHERE L.id = ?";
-
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, new Object[]{id});
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(FIND_BY_ID_QUERY, id);
         return mapLine(result);
     }
 
     public void update(Line newLine) {
         String sql = "update LINE set name = ?, color = ? where id = ?";
-        jdbcTemplate.update(sql, new Object[]{newLine.getName(), newLine.getColor(), newLine.getId()});
+        jdbcTemplate.update(sql, newLine.getName(), newLine.getColor(), newLine.getId());
     }
 
     public List<Line> findAll() {
-        String sql = "select L.id as line_id, L.name as line_name, L.color as line_color, L.extra_fare as extra_fare, " +
-                "S.id as section_id, S.distance as section_distance, " +
-                "UST.id as up_station_id, UST.name as up_station_name, " +
-                "DST.id as down_station_id, DST.name as down_station_name " +
-                "from LINE L \n" +
-                "left outer join SECTION S on L.id = S.line_id " +
-                "left outer join STATION UST on S.up_station_id = UST.id " +
-                "left outer join STATION DST on S.down_station_id = DST.id ";
-
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql);
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(FIND_ALL_QUERY);
         Map<Long, List<Map<String, Object>>> resultByLine = result.stream().collect(Collectors.groupingBy(it -> (Long) it.get("line_id")));
         return resultByLine.entrySet().stream()
                 .map(it -> mapLine(it.getValue()))
