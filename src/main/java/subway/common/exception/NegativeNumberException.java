7b42668c0a82fa42137082150442e9c0b379236a
Sangyeol Kim <syeol.kim@daum.net>
2021-01-28 00:03:46 +0900

    feat: 커스텀 예외 추가

---
+++ src/main/java/subway/common/exception/NegativeNumberException.java
@@ -0,0 +1,7 @@
+package subway.common.exception;
+
+public class NegativeNumberException extends RuntimeException {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
