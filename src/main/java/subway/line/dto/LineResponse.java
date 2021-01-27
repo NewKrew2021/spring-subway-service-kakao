aa6a2ecac912fdf3e31c089ce070d06e6f1e85dc
Ye-Ji-Kim <kyeoji0128@naver.com>
2021-01-27 21:51:09 +0900

    refactor: 생성자 정리 및 MemberController 경로 매핑 수정

--- src/main/java/subway/line/dto/LineResponse.java
+++ src/main/java/subway/line/dto/LineResponse.java
@@ -12,8 +12,6 @@ public class LineResponse {
     private String color;
     private List<StationResponse> stations;
 
-    public LineResponse() {
-    }
 
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
