dbc3b0ecbedeeed4c2577784841b3ca1fe88f417
sumniy <sumniy94@gmail.com>
2021-01-27 16:48:43 +0900

    Refactor: 피드백 반영 - favoriteDao find에서 rowMapper 적용
    rowMapper를 적용하여, 불필요한 stream과 코드를 줄임.

--- src/main/java/subway/favorite/dao/FavoriteDao.java
+++ src/main/java/subway/favorite/dao/FavoriteDao.java
@@ -2,6 +2,7 @@ package subway.favorite.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import subway.favorite.domain.Favorite;
@@ -10,13 +11,20 @@ import subway.station.domain.Station;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 public class FavoriteDao {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
+
+    private final RowMapper<Favorite> rowMapper = (rs, rowNum) ->
+            new Favorite(
+                    rs.getLong("id"),
+                    rs.getLong("member_id"),
+                    new Station(rs.getLong("source_station_id"), rs.getString("source_station_name")),
+                    new Station(rs.getLong("target_station_id"), rs.getString("target_station_name"))
+            );
 
     @Autowired
     public FavoriteDao(JdbcTemplate jdbcTemplate) {
@@ -27,6 +35,7 @@ public class FavoriteDao {
     }
 
     public Favorite insert(Long memberId, Long sourceStationId, Long targetStationId) {
+
         Map<String, Object> params = new HashMap<>();
         params.put("member_id", memberId);
         params.put("source_station_id", sourceStationId);
@@ -45,19 +54,7 @@ public class FavoriteDao {
                 "join STATION TST on F.target_station_id = TST.id " +
                 "WHERE F.member_id = ?";
 
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, memberId);
-        return result.stream()
-                .map(this::mapFavorite)
-                .collect(Collectors.toList());
-    }
-
-    private Favorite mapFavorite(Map<String, Object> it) {
-        return new Favorite(
-                (Long) it.get("FAVORITE_ID"),
-                (Long) it.get("FAVORITE_MEMBER_ID"),
-                new Station((Long) it.get("SOURCE_STATION_ID"), (String) it.get("SOURCE_STATION_NAME")),
-                new Station((Long) it.get("TARGET_STATION_ID"), (String) it.get("TARGET_STATION_NAME"))
-        );
+        return jdbcTemplate.query(sql, rowMapper, memberId);
     }
 
     public void deleteById(Long id) {
