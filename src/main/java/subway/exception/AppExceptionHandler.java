686a90d56f8409c3dffb0e21d1fc0064ea273bd0
jack.won <jack.won@kakaocorp.com>
2021-01-27 14:43:03 +0900

    refactor: 예외 핸들링 개선
    * SQL 테이블 외부 키 설정
    * 중복 및 정합도 경우 다른 예외 메시지 반환하도록 설정

--- src/main/java/subway/exception/AppExceptionHandler.java
+++ src/main/java/subway/exception/AppExceptionHandler.java
@@ -2,13 +2,14 @@ package subway.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import subway.exception.custom.*;
-
-import java.sql.SQLException;
 
 @ControllerAdvice
 public class AppExceptionHandler {
@@ -27,21 +28,31 @@ public class AppExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
-    @ExceptionHandler(SQLException.class)
+    @ExceptionHandler({DuplicateKeyException.class})
+    public ResponseEntity<String> handleDuplicateKeyException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 사용 중인 값입니다.");
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<String> handleDataIntegrityViolationException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 요청입니다.");
+    }
+
+    @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<String> handleSQLException(Exception e) {
-        log.warn("{}\n{}", e.getMessage(), e.getStackTrace());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 입력입니다.");
+        log.error("{}\n{}", e.getMessage(), e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("데이터 처리 중 문제가 발생하였습니다.");
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(Exception e) {
-        log.warn("{}\n{}", e.getMessage(), e.getStackTrace());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("수행 중 문제가 발생하였습니다.");
+        log.error("{}\n{}", e.getMessage(), e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("수행 중 문제가 발생하였습니다.");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        log.warn("{}\n{}", e.getMessage(), e.getStackTrace());
+        log.error("{}\n{}", e.getMessage(), e.getStackTrace());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("요청 처리에 문제가 발생하였습니다.");
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,7 +10,7 @@ create table if not exists LINE
     id         bigint auto_increment not null,
     name       varchar(255)          not null unique,
     color      varchar(20)           not null,
-    extra_fare int,
+    extra_fare int check (extra_fare between 0 and 2000000000),
     primary key (id)
 );
 
@@ -18,10 +18,11 @@ create table if not exists SECTION
 (
     id              bigint auto_increment not null,
     line_id         bigint                not null,
-    up_station_id   bigint                not null,
-    down_station_id bigint                not null,
-    distance        int                   not null,
-    primary key (id)
+    up_station_id   bigint                not null references STATION (id),
+    down_station_id bigint                not null references STATION (id),
+    distance        int                   not null check (distance between 1 and 1000000),
+    primary key (id),
+    constraint fk_section_line_id foreign key (line_id) references Line (id) on delete cascade
 );
 
 create table if not exists MEMBER
@@ -29,7 +30,7 @@ create table if not exists MEMBER
     id       bigint auto_increment not null,
     email    varchar(255)          not null unique,
     password varchar(255)          not null,
-    age      int                   not null,
+    age      int                   not null CHECK (age between 1 and 200),
     primary key (id)
 );
 
@@ -39,5 +40,8 @@ create table if not exists FAVORITE
     member_id         bigint                not null,
     source_station_id bigint                not null,
     target_station_id bigint                not null,
-    primary key (id)
+    primary key (id),
+    constraint fk_favorite_member_id foreign key (member_id) references MEMBER (id) on delete cascade,
+    constraint fk_favorite_source_station_id foreign key (source_station_id) references STATION (id) on delete cascade,
+    constraint fk_favorite_target_station_id foreign key (target_station_id) references STATION (id) on delete cascade
 );
