489aa5aa232f9d2af8e1ef59f9bff3bfeea94df6
kukaro <justkukaro@naver.com>
2021-01-29 16:37:11 +0900

    refactor : 로직 분리를 위한 준비작업(잘못될 경우 여기 직전으로 되돌려야 합니다.)

--- src/main/java/subway/line/domain/DirectedSections.java
+++ src/main/java/subway/line/domain/DirectedSections.java
@@ -2,7 +2,6 @@ package subway.line.domain;
 
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
-import org.jgrapht.graph.WeightedMultigraph;
 import subway.member.domain.LoginMember;
 import subway.member.domain.LoginMemberType;
 import subway.station.domain.Station;
@@ -11,59 +10,30 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class DirectedSections {
+public class DirectedSections extends Section {
     private static final int MIN_DISTANCE = 10;
     private static final int MAX_DISTANCE = 50;
     private static final int DEFAULT_PRICE = 1250;
     private static final int ADDITIONAL_PRICE = 100;
-    private static final int PER_NEAR_DISTANCE = 5;
-    private static final int PER_FAR_DISTANCE = 8;
-    private static final int FARE_FREE_PRICE = 350;
-    private static final int RATE_HALF = 2;
-    private static final int RATE_FIFTH = 5;
     private final List<DirectedSection> sections;
     private final ExtraFare maxExtraFare;
     private final Station source;
     private final Station destination;
     private final DijkstraShortestPath<Station, DefaultWeightedEdge> graph;
 
-    public DirectedSections(SubwayMap subwayMap, Station source, Station destination) {
+    public DirectedSections(List<DirectedSection> sections, Station source, Station destination,DijkstraShortestPath<Station, DefaultWeightedEdge> graph,ExtraFare maxExtraFare) {
+        this.sections = sections;
         this.source = source;
         this.destination = destination;
-        List<Section> sections = subwayMap.findAllSections();
-        graph = createGraph(sections);
-        List<Station> orderedStations = graph.getPath(source, destination).getVertexList();
-        this.sections = new ArrayList<>();
-        for (int i = 0; i < orderedStations.size() - 1; i++) {
-            Station sourceStation = orderedStations.get(i);
-            Station targetStation = orderedStations.get(i + 1);
-            DirectedSection directedSection = new DirectedSection(findSection(sections, sourceStation, targetStation), sourceStation);
-            this.sections.add(directedSection);
-        }
-        this.maxExtraFare = ExtraFare.of(subwayMap, this.sections);
+        this.maxExtraFare = maxExtraFare;
+        this.graph = graph;
     }
 
-    private DijkstraShortestPath<Station, DefaultWeightedEdge> createGraph(List<Section> sections) {
-        WeightedMultigraph<Station, DefaultWeightedEdge> graph =
-                new WeightedMultigraph<>(DefaultWeightedEdge.class);
-
-        for (Section section : sections) {
-            graph.addVertex(section.getUpStation());
-            graph.addVertex(section.getDownStation());
-            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
-        }
-
-        return new DijkstraShortestPath<>(graph);
+    public int getMaxExtraFare() {
+        return maxExtraFare.getValue();
     }
 
-    private Section findSection(List<Section> sections, Station stationA, Station stationB) {
-        return sections.stream()
-                .filter(section -> section.contains(stationA) && section.contains(stationB))
-                .findAny()
-                .orElseThrow(IllegalStateException::new);
-    }
-
-    private int getPrice() {
+    public int getPrice() {
         int distance = getDistance();
         if (distance <= MIN_DISTANCE) {
             return DEFAULT_PRICE + maxExtraFare.getValue();
@@ -74,19 +44,17 @@ public class DirectedSections {
                     maxExtraFare.getValue();
         }
         return DEFAULT_PRICE +
-                (MAX_DISTANCE - MIN_DISTANCE) / PER_NEAR_DISTANCE * ADDITIONAL_PRICE +
+                (MAX_DISTANCE - MIN_DISTANCE) / 5 * ADDITIONAL_PRICE +
                 over50Bonus(distance) * ADDITIONAL_PRICE +
                 maxExtraFare.getValue();
     }
 
-    private int under50Bonus(int distance) {
-        return (distance - MIN_DISTANCE) / PER_NEAR_DISTANCE +
-                ((distance - MIN_DISTANCE) % PER_NEAR_DISTANCE == 0 ? 0 : 1);
+    public int under50Bonus(int distance) {
+        return (distance - MIN_DISTANCE) / 5 + ((distance - MIN_DISTANCE) % 5 == 0 ? 0 : 1);
     }
 
-    private int over50Bonus(int distance) {
-        return (distance - MAX_DISTANCE) / PER_FAR_DISTANCE +
-                ((distance - MIN_DISTANCE) % PER_FAR_DISTANCE == 0 ? 0 : 1);
+    public int over50Bonus(int distance) {
+        return (distance - MAX_DISTANCE) / 8 + ((distance - MIN_DISTANCE) % 8 == 0 ? 0 : 1);
     }
 
     public int getResultPrice(LoginMember loginMember) {
@@ -95,16 +63,16 @@ public class DirectedSections {
             return 0;
         }
         if (loginMember.getType() == LoginMemberType.CHILD) {
-            return defaultPrice - (defaultPrice - FARE_FREE_PRICE) / RATE_HALF;
+            return defaultPrice - (defaultPrice - 350) / 2;
         }
         if (loginMember.getType() == LoginMemberType.ADOLESCENT) {
-            return defaultPrice - (defaultPrice - FARE_FREE_PRICE) / RATE_FIFTH;
+            return defaultPrice - (defaultPrice - 350) / 5;
         }
         return defaultPrice;
     }
 
     public int getDistance() {
-        return (int) graph.getPathWeight(source, destination);
+        return (int) graph.getPathWeight(source,destination);
     }
 
     public List<Station> getStations() {
--- src/main/java/subway/line/domain/SubwayMap.java
+++ src/main/java/subway/line/domain/SubwayMap.java
@@ -1,8 +1,12 @@
 package subway.line.domain;
 
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 import subway.line.exception.LineNotFoundException;
 import subway.station.domain.Station;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -32,7 +36,42 @@ public class SubwayMap {
     }
 
     public DirectedSections getShortestPath(Station source, Station destination) {
-        return new DirectedSections(this, source, destination);
+        List<Section> sections = findAllSections();
+        DijkstraShortestPath<Station, DefaultWeightedEdge> graph = createGraph(sections);
+        List<Station> orderedStations = graph.getPath(source, destination).getVertexList();
+        List<DirectedSection> directedSections = new ArrayList<>();
+
+        for (int i = 0; i < orderedStations.size() - 1; i++) {
+            Station sourceStation = orderedStations.get(i);
+            Station targetStation = orderedStations.get(i + 1);
+            DirectedSection directedSection = new DirectedSection(findSection(sections, sourceStation, targetStation), sourceStation);
+            directedSections.add(directedSection);
+        }
+        return new DirectedSections(directedSections,
+                source,
+                destination,
+                graph,
+                ExtraFare.of(this, directedSections));
+    }
+
+    private DijkstraShortestPath<Station, DefaultWeightedEdge> createGraph(List<Section> sections) {
+        WeightedMultigraph<Station, DefaultWeightedEdge> graph =
+                new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+        for (Section section : sections) {
+            graph.addVertex(section.getUpStation());
+            graph.addVertex(section.getDownStation());
+            graph.setEdgeWeight(graph.addEdge(section.getUpStation(), section.getDownStation()), section.getDistance());
+        }
+
+        return new DijkstraShortestPath<>(graph);
+    }
+
+    private Section findSection(List<Section> sections, Station stationA, Station stationB) {
+        return sections.stream()
+                .filter(section -> section.contains(stationA) && section.contains(stationB))
+                .findAny()
+                .orElseThrow(IllegalStateException::new);
     }
 
     public void refresh(List<Line> lines) {
