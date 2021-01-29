12329a861d21554b4215d9292873cc8fb905aa76
SimJung <tlawjd99gma@gmail.com>
2021-01-29 20:57:31 +0900

    feat : Interceptor 과정 체크 로직 추가
     - jwtTokenProvider의 validation 메소드를 사용하여 토큰 적합성 판단 로직 추가

--- src/main/java/subway/auth/ui/LoginInterceptor.java
+++ src/main/java/subway/auth/ui/LoginInterceptor.java
@@ -2,6 +2,7 @@ package subway.auth.ui;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import subway.auth.infrastructure.AuthorizationExtractor;
+import subway.auth.infrastructure.JwtTokenProvider;
 import subway.exception.InvalidTokenException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +12,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String accessToken = AuthorizationExtractor.extract(request);
-        if(accessToken == null){
+        if(accessToken == null || !new JwtTokenProvider().validateToken(accessToken)){
             throw new InvalidTokenException();
         }
         return super.preHandle(request, response, handler);
