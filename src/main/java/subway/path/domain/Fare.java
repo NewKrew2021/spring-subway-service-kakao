c50d8a3825b9eef7333c4e28247ad5b240c10c07
dy.namic <dy.namic@kakaocorp.com>
2021-01-29 18:53:09 +0900

    refactor(Fare) : 거리에 따른 요금계산하는 기능 리팩터링
    1) 수식을 풀어서 알기 쉽게 코딩
    2) 하드코딩된 숫자들을 상수화

--- src/main/java/subway/path/domain/Fare.java
+++ src/main/java/subway/path/domain/Fare.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Fare {
     private static final int BASIC_FARE = 1250;
+    private static final int FIRST_PART_LIMIT = 10;
+
+    private static final int SECOND_PART_LIMIT = 50;
+    private static final int SECOND_PART_UNIT_FARE = 100;
+    private static final int SECOND_PART_UNIT_DISTANCE = 5;
+
+    private static final int THIRD_PART_LIMIT = Integer.MAX_VALUE;
+    private static final int THIRD_PART_UNIT_FARE = 100;
+    private static final int THIRD_PART_UNIT_DISTANCE = 8;
+
     private static final int MIN_AGE_OF_CHILD = 6;
     private static final int MIN_AGE_OF_TEEN = 13;
     private static final int MIN_AGE_OF_ADULT = 19;
@@ -13,9 +23,20 @@ public class Fare {
     }
 
     private static int calculateByDistance(int distance) {
-        return BASIC_FARE +
-                (int)Math.ceil(Math.min(Math.max((distance - 10), 0), 40 ) / 5.0) * 100 +
-                (int)Math.ceil(Math.max((distance - 50), 0) / 8.0) * 100;
+        return BASIC_FARE
+                + getFareOfEachPartionWith(distance, FIRST_PART_LIMIT, SECOND_PART_LIMIT, SECOND_PART_UNIT_DISTANCE, SECOND_PART_UNIT_FARE)
+                + getFareOfEachPartionWith(distance, SECOND_PART_LIMIT, THIRD_PART_LIMIT, THIRD_PART_UNIT_DISTANCE, THIRD_PART_UNIT_FARE);
+    }
+
+    private static int getFareOfEachPartionWith(int totalDistance, int previousPartLimit, int currentPartLimit, int currentPartUnitDistance, int currentPartUnitFare) {
+        //get distance usage in partition
+        int distanceUsage = Math.max(0, Math.min(totalDistance, currentPartLimit) - previousPartLimit);
+
+        //get count of unit to be used in this partition
+        int unitCount = (distanceUsage + currentPartUnitDistance - 1) / currentPartUnitDistance;
+
+        //calculate fare for current partition
+        return unitCount * currentPartUnitFare;
     }
 
     private static int calculateExtraFareByLine(List<Integer> extraFareList) {
